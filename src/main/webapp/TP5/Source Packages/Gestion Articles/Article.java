42a1f8101ff3999d346eac9189791d5385af073d
Imad Asserne <imad.asserne@gmail.com>
2023-03-09 14:58:29 +0000

    Add new attribute

--- src/main/webapp/TP5/Source Packages/Gestion Articles/Article.java
+++ src/main/webapp/TP5/Source Packages/Gestion Articles/Article.java
@@ -3,20 +3,23 @@ public class Article{
 private int code_articlr;
 private String designation;
 private double prix_uni;
+private int qte_stock;
 
-    public Article(int code_articlr, String designation, double prix_uni) {
+    public Article(int code_articlr, String designation, double prix_uni,int qte_stock) {
         this.code_articlr = code_articlr;
         this.designation = designation;
         this.prix_uni = prix_uni;
+        this.qte_stock=qte_stock;
     }
 
     public Article(int code_articlr) {
         this.code_articlr = code_articlr;
     }
 
-    public Article(String designation, double prix_uni) {
+    public Article(String designation, double prix_uni,int qte_stock) {
         this.designation = designation;
         this.prix_uni = prix_uni;
+        this.qte_stock=qte_stock;
     }
 
     public Article() {
@@ -46,12 +49,21 @@ private double prix_uni;
         this.prix_uni = prix_uni;
     }
 
+    public int getQte_stock() {
+        return qte_stock;
+    }
+
+    public void setQte_stock(int qte_stock) {
+        this.qte_stock = qte_stock;
+    }
+
     @java.lang.Override
     public java.lang.String toString() {
         return "Article{" +
                 "code_articlr=" + code_articlr +
                 ", designation='" + designation + '\'' +
                 ", prix_uni=" + prix_uni +
+                ", qte_stock=" + qte_stock +
                 '}';
     }
 }

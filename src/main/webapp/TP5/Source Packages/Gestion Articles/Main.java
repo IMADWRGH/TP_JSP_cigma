7bcc5ee046a94f324d3110184b63c3d404cc4f3b
Imad Asserne <imad.asserne@gmail.com>
2023-03-10 23:49:08 +0000

    change path

--- src/main/webapp/TP5/Source Packages/Gestion Articles/Main.java
+++
@@ -1,7 +0,0 @@
-package Gestion Articles;
-public class Main {
-    public static void main(String[] args) {
-        GestionArticles gestionArticles=new GestionArticles();
-        gestionArticles.Ajouter(1,"PC ASUS",35000,50);
-    }
-}
